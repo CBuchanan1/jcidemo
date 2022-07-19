6a4ddab1c2b6ef9520192c523f7c16405aa8d10d
CBuchanan1 <100205423+CBuchanan1@users.noreply.github.com>
2022-07-19 15:15:02 +0100

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+package uk.ac.qub.eeecs.dave;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a+b+1;
+	}
+	
+	public int sub(int a, int b) {
+		return a-b;
+	} 
+	
+	public String print(String words) {
+		
+		System.out.println(words);
+		return words;
+	}
+	
+}
